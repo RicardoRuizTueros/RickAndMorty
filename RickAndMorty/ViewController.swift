f1fe322be9610f5dde4b75c86027baf9db0cd84c
Ricardo Ruiz <ricardo.ruiz.tueros@icloud.com>
2019-12-12 15:59:39 +0100

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Ricardo Ruiz on 12/12/2019.
+//  Copyright © 2019 Ricardo Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
